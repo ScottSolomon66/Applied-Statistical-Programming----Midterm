dbfdf168bac18b750732e73a3cf3dd546634e8ab
ScottSolomon66 <scott.solomon66@gmail.com>
2016-03-22 23:51:39 -0500

    Successfully functionalized plot for trapezoids
    Wrote a function that plots the trapezoids for the trapezoidal
    estimation.

--- plot.R
+++ plot.R
@@ -20,20 +20,51 @@ setMethod(f = "plot",
                  y = NULL,
                  xlab = "x",
                  ylab = "y",
+                 main = "Trapezoidal approximations of an integral",
                  xlim = c(min(x@x_values),max(x@x_values)),
                  ylim = c(min(x@y_values),max(x@y_values)))
             x_values<-x@x_values
             y_values<-x@y_values
             n<-length(x_values)
-            segments(x_values[2], y_values[2], x_values[3], y_values[3])
-            segments(x_values[2], 0, x_values[3], 0)
-            segments(x_values[2], y_values[2], x_values[2], 0)
-            segments(x_values[3], y_values[3], x_values[3], 0)
+            for (i in 1:n){
+              segments(x_values[i], y_values[i], x_values[(i+1)], y_values[(i+1)])
+              segments(x_values[i], 0, x_values[(i+1)], 0)
+              segments(x_values[i], y_values[i], x_values[i], 0)
+              segments(x_values[(i+1)], y_values[(i+1)], x_values[(i+1)], 0)
+            }
           })
+#' @export
+setMethod(f = "plot",
+          signature = "int_est.Simpson",
+          definition = function(x = NULL, y = x, ...){
+            plot(x = NULL,
+                 y = NULL,
+                 xlab = "x",
+                 ylab = "y",
+                 main = "Simpson approximations of an integral",
+                 xlim = c(min(x@x_values),max(x@x_values)),
+                 ylim = c(min(x@y_values),max(x@y_values)))
+            x_values<-x@x_values
+            y_values<-x@y_values
+            n<-length(x_values)}
+          )
+
+
+
+
+
+plot(test_int_est3)
+
+test_int_est3<-new("int_est.Simpson", x_values = a, y_values = b, estimation = 50)
+
+test_int_est2<-new("int_est.Trapezoid", x_values = a, y_values = b, estimation = 50)
+
+a<-c(0:10)
+b<-sin(a)
 
 ?segments
 
-plot(test_int_est)
+plot(test_int_est2)
 
 x<-c(0:10)
 y<-c(0:10)
