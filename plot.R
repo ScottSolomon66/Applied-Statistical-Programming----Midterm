17208ad726e9bbc6d36c32d3cf2aff1cf7724213
ScottSolomon66 <scott.solomon66@gmail.com>
2016-03-22 22:50:44 -0500

    Plot
    Starting to work on the plot function.

---
+++ plot.R
@@ -0,0 +1,26 @@
+#' plots the estimatations for an object of class int_est
+#'
+#' plots the estimation trapezoids and parabolas for objects of class int_est
+#'
+#' @param int_est_object An object of class int_est
+#'
+#' @return plots the estimation trapezoids and parabolas for objects of class int_est
+#'  \item{plot}{the plot of the estimations}
+#' @author Scott Solomon
+#' @note This function gives a visual display of the simpson and trapezoid rules
+#' @examples
+#' 
+#' @rdname plot
+#' @aliases plot
+#' @export
+setMethod(f = "plot",
+          signature = "int_est.Trapezoid",
+          definition = function(x = NULL, y = x){
+          plot(xlim = (max(x@x_values)
+          }
+          )
+          
+max(test_int_)
+test_int_est
+
+plot(test_int_est)
